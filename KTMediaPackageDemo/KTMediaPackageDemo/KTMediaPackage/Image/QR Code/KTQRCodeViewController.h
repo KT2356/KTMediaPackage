25aeca92046ef4dc8b47e787a832e27375c25ae4
KT2356 <15152112356@163.com>
2015-12-22 11:38:20 +0800

    initial submit

---
+++ KTMediaPackageDemo/KTMediaPackageDemo/KTMediaPackage/Image/QR Code/KTQRCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  KTQRCodeViewController.h
+//  KTMediaPackageDemo
+//
+//  Created by KT on 15/12/22.
+//  Copyright © 2015年 KT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KTQRCodeViewController : UITableViewController
+
+@end
---
+++ KTMediaPackageDemo/KTMediaPackageDemo/KTMediaPackage/Image/QR Code/KTQRCodeViewController.m
@@ -0,0 +1,13 @@
+//
+//  KTQRCodeViewController.m
+//  KTMediaPackageDemo
+//
+//  Created by KT on 15/12/22.
+//  Copyright © 2015年 KT. All rights reserved.
+//
+
+#import "KTQRCodeViewController.h"
+
+@implementation KTQRCodeViewController
+
+@end
