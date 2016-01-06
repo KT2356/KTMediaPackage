237ad8487be19e2fac45e504149936436ff72fae
KT2356 <15152112356@163.com>
2016-01-06 14:33:31 +0800

    videoPlayer finished

--- KTMediaPackageDemo/KTMediaPackageDemo/ViewController.m
+++ KTMediaPackageDemo/KTMediaPackageDemo/ViewController.m
@@ -27,7 +27,10 @@
 #pragma mark - VideoPlayer
 - (IBAction)showVideoPlayer:(UIButton *)sender {
     NSString *urlString = @"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
-    KTVideoPlayer *videoView = [[KTVideoPlayer alloc] initWithFrame:CGRectMake(0, 200, [UIScreen mainScreen].bounds.size.width, 200) urlString:urlString];
+    KTVideoPlayer *videoView = [[KTVideoPlayer alloc]
+                                initWithFrame:CGRectMake(0, 200, [UIScreen mainScreen].bounds.size.width, 200)
+                                urlString:urlString
+                                loadingImage:[UIImage imageNamed:@"loading"]];
     videoView.delegate = self;
     [self.view addSubview:videoView];
 }
